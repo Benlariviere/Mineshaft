3a0567914a86e4284d281c9badddbdcd6e6ee189
Benlariviere <75094149+Benlariviere@users.noreply.github.com>
2021-11-16 11:27:58 -0500

    GestionMenu + Amélioration Cristaux

---
+++ MineshaftProject/Assets/Scripts/GestionMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GestionMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Jouer() // Efface toutes les données du jeu et crée une nouvelle partie
+    {
+       
+        SceneManager.LoadScene(1);
+
+    }
+    public void QuitterJeu() // Permet de quitter le jeu
+    {
+        Application.Quit();
+    }
+}
