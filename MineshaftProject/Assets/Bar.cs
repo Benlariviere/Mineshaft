c552407e39693b1c038a72605f4828eab57fb59d
Louis Nicolas <shinoclabl@gmail.com>
2021-11-17 12:20:42 -0500

    Animation UI + Fine tune global amb
    Animation UI + Fine tune global amb

---
+++ MineshaftProject/Assets/Bar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+
+public class Bar : MonoBehaviour
+{
+
+    public GameObject bar;
+    public int time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AnimBar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AnimBar()
+    {
+        LeanTween.scaleX(bar, -1, time);
+
+    }
+
+
+}
